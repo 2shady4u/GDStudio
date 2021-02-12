49ccab2794ba3f326cc5872a33a59cd9d8c02aa0
sdtv9507 <apr8950@gmail.com>
2021-02-12 18:46:08 -0500

    Part of the CodeEditor script was redone

--- addons/cpp_file_editor/Scripts/CodeEditor.gd
+++ addons/cpp_file_editor/Scripts/CodeEditor.gd
@@ -18,12 +18,14 @@ var keywords = ["auto", "short", "struct", "unsigned",
 "catch", "false", "operator", "template", "typename",
 "class", "friend", "private", "this", "using",
 "const_cast", "inline", "public", "throw", "virtual",
-"delete", "mutable", "protected", "true", "wchar_t"]
-var value_types = ["const", "int", "float", "double", "char"]
+"delete", "mutable", "protected", "true", "wchar_t",
+"const", "int", "float", "double", "char", "string"]
+var error_type = -1
 var error_types = {
 "line_end": "Expected ; at the end of the statement"
 }
-var operators = ["+", "-", "*", "/", "=", "%"]
+var token_line_error = ""
+var operators = ["+", "-", "*", "/", "=", "%", "<<", ">>"]
 var variable_tracker = []
 var error_text = ""
 var current_content = ""
@@ -31,6 +33,7 @@ var file_name = ""
 var in_function = false
 var text_changed = false
 var has_error = false
+var showing_error = false
 var error_line = -1
 var shader
 
@@ -52,9 +55,6 @@ func setup_tokens():
 	for i in keywords:
 		$Container/CodeEditor.add_keyword_color(i, Color8(128,0,255,255))
 	
-	for i in value_types:
-		$Container/CodeEditor.add_keyword_color(i, Color8(32,0,224,255))
-		
 
 func get_content():
 	return $Container/CodeEditor.text
@@ -68,44 +68,94 @@ func _on_CodeEditor_text_changed():
 		text_changed = false
 	else:
 		text_changed = true
-
-func line_end_rules():
 	line_number = $Container/CodeEditor.get_line_count()
-	if has_error == false:
-		for i in line_number:
-			current_line = $Container/CodeEditor.get_line(i)
-			if not current_line.empty():
-				if not current_line.begins_with("#include") and not current_line.begins_with("//"):
-					if is_function(current_line) == false:
-						if not current_line.ends_with(";"):
-							has_error = true
-							error_line = i
-							$Container/CodeEditor/ErrorColor.set_position(Vector2(0, 2 + (error_line * 18)))
-							$Container/CodeEditor/ErrorColor.show()
-							error_text = "\n"+error_types["line_end"]
-							$VBoxContainer/ErrorLog/ErrorLog.text += error_text
-							break
-	else:
-		update_error_pos()
-		current_line = $Container/CodeEditor.get_line(error_line)
-		if not current_line.empty():
-			if not current_line.begins_with("#include") and not current_line.begins_with("//"):
-				if is_function(current_line) == false:
-					if current_line.ends_with(";"):
-						has_error = false
-						error_line = -1
-						$Container/CodeEditor/ErrorColor.hide()
-						$Container/CodeEditor/ErrorColor.set_position(Vector2(0, 2 + (error_line * 18)))
-						var pos = $VBoxContainer/ErrorLog/ErrorLog.text.find(error_text)
-						var length = error_text.length()
-						var text = $VBoxContainer/ErrorLog/ErrorLog.text
-						text.erase(pos, length)
-						$VBoxContainer/ErrorLog/ErrorLog.text = text
-						error_text = ""
+	variable_tracker = []
+	$VBoxContainer/VariableLog/VariableLog.text = "Variables"
+	for i in line_number:
+		tokenize(i)
+		check_variables(i)
+	if error_type > -1:
+		check_errors(token_line_error, error_line)
+	show_errors()
 
-func update_error_pos():
-	var updated_pos = error_line - $Container/CodeEditor.scroll_vertical
-	$Container/CodeEditor/ErrorColor.set_position(Vector2(0, 2 + (updated_pos * 18)))
+func parse_line(line: int):
+	var current_line = $Container/CodeEditor.get_line(line)
+	var comments = current_line.find("//", 0)
+	if comments > -1:
+		current_line.erase(comments, 999)
+	var correct_line = current_line.strip_edges()
+	return correct_line
+
+func tokenize(line):
+	var correct_line = parse_line(line)
+	var line_split = correct_line.split(" ")
+	var token = ""
+	var is_string = false
+	for value in line_split:
+		if is_string == true:
+			token += "s"
+			if value.ends_with("\""):
+				is_string = false
+		else:
+			if value.begins_with("\""):
+				is_string = true
+				token += "s"
+			if value.begins_with("#"):
+				token += "p"
+			elif keywords.has(value):
+				token += "k"
+			elif operators.has(value):
+				token += "o"
+			elif value.is_valid_integer() or value.is_valid_float():
+				token += "n"
+			else:
+				if $Container/CodeEditor.get_line(line) != "":
+					token += "c"
+	#print(line)
+	#print(token)
+	#final semicolon
+	if has_error == false:
+		var code = token.find("c", 0)
+		if code > 0:
+			if token[code - 1] == "k" and not correct_line[correct_line.length() - 1].ends_with(")"):
+				if correct_line[correct_line.length() - 1] != ";":
+					has_error = true
+					error_line = line
+					error_type = 0
+					token_line_error = token
+
+func check_errors(token: String, line: int):
+	match error_type:
+		0:
+			var correct_line = parse_line(line)
+			var code = token.find("c", 0)
+			if token[code - 1] == "k":
+				if correct_line[correct_line.length() - 1] == ";":
+					has_error = false
+					error_line = -1
+					error_type = -1
+					token_line_error = ""
+
+func show_errors():
+	if has_error == true:
+		if showing_error == false:
+			var error_pos = error_line - $Container/CodeEditor.scroll_vertical
+			$Container/CodeEditor/ErrorColor.set_position(Vector2(0, 2 + (error_pos * 20)))
+			$Container/CodeEditor/ErrorColor.show()
+			error_text = "\n"+error_types["line_end"]
+			$VBoxContainer/ErrorLog/ErrorLog.text += error_text
+			showing_error = true
+	else:
+		if showing_error == true:
+			$Container/CodeEditor/ErrorColor.hide()
+			$Container/CodeEditor/ErrorColor.set_position(Vector2(0, 2 + (error_line * 20)))
+			var pos = $VBoxContainer/ErrorLog/ErrorLog.text.find(error_text)
+			var length = error_text.length()
+			var text = $VBoxContainer/ErrorLog/ErrorLog.text
+			text.erase(pos, length)
+			$VBoxContainer/ErrorLog/ErrorLog.text = text
+			error_text = ""
+			showing_error = false
 
 func is_function(line: String):
 	if in_function == true:
@@ -117,23 +167,22 @@ func is_function(line: String):
 	
 	var split_line = line.split(" ")
 	if split_line.size() > 2:
-		if value_types.has(split_line[0]) and split_line[1].ends_with(")"):
+		if keywords.has(split_line[0]) and split_line[1].ends_with(")"):
 			in_function = true
 			return true
 		else:
 			return false
 
-func check_variables():
-	line_number = $Container/CodeEditor.get_line_count()
-	for i in line_number:
-		var line = $Container/CodeEditor.get_line(i)
-		var line_split = line.split(" ")
-		if line_split.size() > 2:
-			if value_types.has(line_split[0]) and operators.has(line_split[2]):
-				var variable_data = [line_split[0], line_split[1]]
-				if find_arrays(variable_data, variable_tracker) == false:
-					variable_tracker.append(variable_data)
-					$VBoxContainer/VariableLog/VariableLog.text += "\n" + variable_data[1] + ": " + variable_data[0]
+func check_variables(line):
+	var current_line = $Container/CodeEditor.get_line(line)
+	var correct_line = current_line.strip_edges()
+	var line_split = correct_line.split(" ")
+	if line_split.size() > 2:
+		if keywords.has(line_split[0]) and operators.has(line_split[2]):
+			var variable_data = [line_split[0], line_split[1]]
+			if find_arrays(variable_data, variable_tracker) == false:
+				variable_tracker.append(variable_data)
+				$VBoxContainer/VariableLog/VariableLog.text += "\n" + variable_data[1] + ": " + variable_data[0]
 
 func find_arrays(search_array: Array, main_array: Array):
 	var total = search_array.size()
@@ -153,16 +202,7 @@ func auto_add():
 		print("yes")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#time += delta
-	auto_add()
-	line_end_rules()
-	check_variables()
-#	if time >= 1:
-#		current_content = $Container/CodeEditor.text
-#		var parsed_content = current_content.split(" ")
-#		for i in parsed_content:
-#			if tokens.has(i):
-#				current_content.replace(String(i) , "yes")
-#		$Container/CodeEditor.text = current_content
+#func _process(delta):
+#	time += delta
+#	if time >= 0.8:
 #		time = 0
