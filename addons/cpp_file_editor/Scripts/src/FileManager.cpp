5ad62e69a89b3649219e5bfef2e890f3cb937887
sdtv9507 <apr8950@gmail.com>
2021-03-26 09:12:58 -0400

    Fix an error when closing tabs

--- addons/cpp_file_editor/Scripts/src/FileManager.cpp
+++ addons/cpp_file_editor/Scripts/src/FileManager.cpp
@@ -68,11 +68,11 @@ void EditorFile::open_file(String path)
     file->free();
     ((Tabs *)get_node("TabContainer"))->add_child(new_instanced_scene, true);
     cast_to<CodeEditor>(new_instanced_scene)->set_initial_content(content);
-    this->tab_number = ((Tabs *)get_node("TabContainer"))->get_child_count() - 1;
+    this->tab_number = ((Tabs *)get_node("TabContainer"))->get_child_count();
     this->file_path = path;
     this->file_name = path.split("//")[1];
     ((Tabs *)get_node("TabContainer"))->add_tab(this->file_name);
-    ((Tabs *)get_node("TabContainer"))->set_current_tab(this->tab_number);
+    ((Tabs *)get_node("TabContainer"))->set_current_tab(this->tab_number - 1);
 }
 
 void EditorFile::save_file()
@@ -141,12 +141,11 @@ void EditorFile::_on_TabContainer_tab_changed(int tab)
 void EditorFile::_on_TabContainer_tab_close(int tab)
 {
     this->instance_defined = false;
-    this->current_editor_instance->hide();
-    this->current_editor_instance->queue_free();
-    this->tab_number -= 1;
     this->file_name = "";
     this->file_path = "";
-    if (this->tab_number > 0)
+    ((Tabs *)get_node("TabContainer"))->get_child(tab)->queue_free();
+    this->tab_number = ((Tabs *)get_node("TabContainer"))->get_child_count();
+    if (this->tab_number > 1)
     {
         if (tab == 0)
         {
