2cab74793599208429c7661c660f644da3da207d
sdtv9507 <apr8950@gmail.com>
2021-04-19 15:57:53 -0400

    Improve rust's test file

--- godot.rs
+++ godot.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug)]
+pub struct SomeClass {
+	var: i32,
+}
+
+impl SomeClass {
+	pub fn new() -> Self {
+		SomeClass {
+			var: 0,
+		}
+	}
+	
+	fn some_method(&mut self) {
+		println!("Some Class method");
+	}
 }
